ab2ed73caa382ad3334aa7fc046d99fd112ebce9
Ashfaaq10 <muhammad.eathally5@umail.uom.ac.mu>
2024-07-14 11:58:16 +0400

    changed index.js

--- api/index.js
+++ api/index.js
@@ -3,18 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5005;
+const port = process.env.PORT || 5005;
 
-// Middleware
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cors());
 
-// Routes
 const uploadRoute = require('./upload');
 const uploadBase64Route = require('./upload-base64');
 
-app.use('/upload', uploadRoute);
-app.use('/upload-base64', uploadBase64Route);
+app.use('/api/upload', uploadRoute);
+app.use('/api/upload-base64', uploadBase64Route);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
